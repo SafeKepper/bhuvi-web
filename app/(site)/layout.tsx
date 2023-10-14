532a26ed11204860d8acc7a768afb81c2e7e68c7
Bhuvan <copycattho@gmail.com>
2023-10-14 21:03:48 +0530

    0.0149v
    Accommodating code blocks

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -14,7 +14,7 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
-      <body className="max-w-3xl mx-auto py-10 bg-gray-900">
+      <body className="max-w-4xl mx-auto py-10 bg-gray-900">
         {/* <header className="flex items-center justify-between">
           <Link href="/" className="bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% bg-clip-text text-transparent text-lg font-bold">BhuvaN</Link>
           <Link href={'./RESUME'} className="text-cyan-400 flex items-center gap-5 text-lg font-bold">
