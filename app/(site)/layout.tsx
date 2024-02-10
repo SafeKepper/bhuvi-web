cdfb34a14ec7248c4fbb7a749cbf6e76a727e2b1
Bhuvaneshwaran R <copycattho@gmail.com>
2024-02-10 17:50:09 +0530

    Update layout.tsx

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -16,7 +16,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <link rel="icon" href="https://img.icons8.com/doodle/96/winner.png" sizes="any" />
-      <body className="max-w-4xl mx-auto py-1 pl-12 bg-white">
+      <body className="max-w-4xl mx-auto py-1 pl-8 bg-white">
         {/* <header className="flex items-center justify-between">
           <Link href="/" className="bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% bg-clip-text text-transparent text-lg font-bold">BhuvaN</Link>
           <Link href={'./RESUME'} className="text-cyan-400 flex items-center gap-5 text-lg font-bold">
