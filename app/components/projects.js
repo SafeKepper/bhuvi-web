caeb5f8039ba4fea7ba6cb8733fef4cbdcc1a199
Bhuvan <copycattho@gmail.com>
2024-05-02 17:08:05 +0530

    Update projects.js

--- app/components/projects.js
+++ app/components/projects.js
@@ -14,6 +14,25 @@ export default function Projects() {
             <h2 className="text-black font-semibold antialiased italic">Projects</h2>
         </div>
       <section className="flex flex-col items-start justify-left mt-12 text-white">
+      <div>
+            <div className="w-5/6 h-auto bg-white mb-2 p-2">
+                <h4 className="text-black font-semibold italic mb-2">
+                Analysis of HR Survey Data Employee Turnover Prediction Using Machine Learning (Capstone Project)
+                </h4>
+                <div className="flex flex-row">
+                <Image className="m-3" width="200" height="200" src={pIC50} alt="linkedin--v2"/>
+                </div>
+                <p className="text-black">
+                The goal of this project was to create an ML classification model and conclude on a best performing machine learning model (Decision Tree, Random Forest, Xgboost) to predict employee turnover left or stay.
+                The final XGBoost model performed with a 94.5% f1 score, 97.1% precision score, and 92.1% recall score, determining what features were most important in segregating employees who left and stayed in the company based on the model, The main contributing factors were tenure duration, high work hours, satisfaction level, and total project contribution.
+                    
+                </p>
+                <Link className="text-black mt-4 font-mono" target="_blank" href={"https://app.datacamp.com/workspace/w/eafc4a33-5521-4a15-ab9c-57932657922e/edit/EDA%20-%20Drug%20Discovery%20.ipynb"}>
+                    Visit the Project..
+                </Link>
+
+            </div>
+        </div>
         <div>
             <div className="w-5/6 h-auto bg-white mb-2 p-2">
                 <h4 className="text-black font-semibold italic mb-2">
